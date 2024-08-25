ab5fb3529def9bd513a649aea60375b4ef11b050
Maksim Lapin <katafoxi@gmail.com>
2024-08-25 21:48:55 +0300

    rename

--- cfg/gopro10_bno055/bno055_rosbag_record_entrypoint.sh
+++ cfg/gopro10_bno055/bno055_rosbag_record_entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 # setup ros environment
 if [ -z "${SETUP}" ]; then
     # basic ros environment
-    source "/opt/ros/$ROS_DISTRO/setup.bash"
+    source "/opt/ros/${ROS_DISTRO}/setup.bash"
     
 else
     #from environment variable; should be a absolute path to the appropriate workspaces's setup.bash
@@ -14,6 +14,7 @@ fi
 source "/opt/ros/overlay_ws/devel/setup.bash"
 export ROS_HOSTNAME=localhost
 export ROS_MASTER_URI=http://localhost:11311
+
 #roslaunch rovio rosbagplay_node.launch
 roslaunch rovio bno055_rosbag_record.launch
 

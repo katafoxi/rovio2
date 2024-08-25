5f55424094ca635b1fb514bc07715ecf3cf781af
Maksim Lapin <katafoxi@gmail.com>
2024-08-25 17:40:08 +0300

    small changes

--- cfg/gopro10/UP.sh
+++ cfg/gopro10/UP.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo chmod 666 /dev/i2c-1    # https://answers.ros.org/question/397084/i2c-device-open-failed/
-XAUTH=./.docker.xauth
-touch $XAUTH
-xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+# Сreating a .xauth file to add to the docker container
+# https://www.baeldung.com/linux/docker-container-gui-applications#4-handling-problems-with-the-xauthority-file-different-user-id
+touch "$XAUTH"
+xauth nlist "$DISPLAY" | sed -e 's/^..../ffff/' | xauth -f "$XAUTH" nmerge -
 
 docker compose up
